8536dd810895f06b9358b7d3f5f62c8098abf280
manuelguido <manuelguido.m@gmail.com>
2019-11-08 21:43:03 -0300

    cambios de front en el panel

--- flaskps/static/js/main.js
+++ flaskps/static/js/main.js
@@ -17,30 +17,3 @@ function showAddNote() {
     var newNote = document.getElementById('add-note');
     newNote.style.display = "block";
 }
-
-//Toggle de menu del panel de administración
-function panelToggle() {
-    function switchIcon(){
-        var button = document.getElementById("panel-toggle");
-        if (button.classList.contains("fa-bars")){
-            button.classList.remove("fa-bars");
-            button.classList.add("fa-arrow-left");
-        }
-        else {
-            button.classList.remove("fa-arrow-left");
-            button.classList.add("fa-bars");
-        }
-    }
-    
-    var menu = document.getElementById("panel-menu");
-    var smenu = document.getElementById("panel-menu-inner");
-    if (menu.classList.contains('hide')) {
-        menu.classList.remove("hide");
-        smenu.classList.remove("display-n");
-    }
-    else{
-        menu.classList.add("hide");
-        smenu.classList.add("display-n");
-    }
-    switchIcon();
-}
