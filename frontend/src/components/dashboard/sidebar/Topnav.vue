275e82e3662e55cab9f150216f15899f8014a534
manuelguido <manuelguido.m@gmail.com>
2020-06-14 18:47:22 -0300

    profile data on dashboard

--- frontend/src/components/dashboard/sidebar/Topnav.vue
+++ frontend/src/components/dashboard/sidebar/Topnav.vue
@@ -1,12 +1,16 @@
 <template>
   <mdb-navbar id="topnav" class="shadow-none m-0" dark>
     <mdb-navbar-brand>
-      <span class="h6 white-a w300 ls02 my-3">Manuel</span>
+      <span v-if="user" class="h6 white-a w300 ls02 my-3">{{user.name}} {{user.lastname}}</span>
+      <div v-else-if="loading" class="spinner-border color-c my-0" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
     </mdb-navbar-brand>
   </mdb-navbar>
 </template>
 
 <script>
+import axios from 'axios'
 import { mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem, mdbDropdown, mdbDropdownMenu, mdbDropdownToggle, mdbInput, mdbDropdownItem } from 'mdbvue'
 export default {
   name: 'NavbarPage',
@@ -21,6 +25,36 @@ export default {
     mdbDropdownToggle,
     mdbDropdownItem,
     mdbInput
+  },
+  data () {
+    return {
+      user: false,
+      loading: true
+    }
+  },
+  methods: {
+    // Obtener las rutas del usuario
+    getUserData: function () {
+      const path = '/api/user/profile'
+      axios.get(path).then((res) => {
+        this.user = res.data
+        localStorage.setItem('user', JSON.stringify(this.user))
+        this.loading = false
+      }).catch((error) => {
+        console.log(error + 'Retring')
+        this.getUserData()
+      })
+    }
+  },
+  mounted () {
+    if (localStorage.user) {
+      var loc = localStorage.getItem('user')
+      var result = JSON.parse(loc)
+      this.user = result
+      this.loading = false
+    } else {
+      this.getUserData()
+    }
   }
 }
 </script>
